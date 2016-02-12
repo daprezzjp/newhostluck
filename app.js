c79fc1bfcfe35458d2b47de1a5114a69154f1183
Randy Holeman <rholeman@ucsd.edu>
2016-02-12 12:50:15 -0800

    testing

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
-var project = require('./routes/project');
 // Example route
 // var user = require('./routes/user');
 
@@ -37,8 +36,6 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
-app.get('/project', project.viewProject);
-app.get('/project/:name', project.viewProject);
 // Example route
 // app.get('/users', user.list);
 
--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,5 @@
+var foodhost = require("../json/foodhost.json");
 
-/*
- * GET home page.
- */
-
- exports.view = function(req, res){
-
-   res.render('index', {
-     'projects': [
-       { 'name': 'Pot Pie',
-         'image': 'http://goo.gl/1v48ya',
-         'id': 'project1'
-       },
-       { 'name': 'Yogurt',
-         'image': 'https://goo.gl/f2NsPU',
-         'id': 'project2'
-       },
-       { 'name': 'Pop Corn',
-         'image': 'https://goo.gl/TRa1HR',
-         'id': 'project3'
-       },
-       { 'name': 'Sangria',
-         'image': 'http://goo.gl/DZjkCg',
-         'id': 'project4'
-       },
-       { 'name': 'Spaghetti',
-         'image': 'http://goo.gl/7CtUHm',
-         'id': 'project5'
-       },
-       { 'name': 'Sushi',
-         'image': 'https://goo.gl/HK51RO',
-         'id': 'project6'
-       },
-       { 'name': 'Hot Dogs',
-         'image': 'http://goo.gl/bN2axu',
-         'id': 'project7'
-       },
-       { 'name': 'Hamburgers',
-         'image': 'http://goo.gl/GtuQ76',
-         'id': 'project8'
-       }
-     ]
-   }
- )
- };
+exports.view = function(req, res){
+  res.render('index', foodhost);
+};
